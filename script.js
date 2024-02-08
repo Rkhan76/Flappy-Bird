9ea2ccc857f36672bf0a2722f98085eaff668c27
Rakhshan khan <rakshaankhan1@gmail.com>
2024-02-08 23:45:10 +0530

    Add game background image

---
+++ script.js
@@ -0,0 +1,27 @@
+let board
+let boardWidth = 360
+let boardHeight = 640
+
+
+//bird
+let birdWidth = 34;
+let birdHeight = 24;
+let birdX = boardWidth/8;
+let birdY = boardHeight/2;
+
+let bird ={
+    x: birdX,
+    y: birdY,
+    width: birdWidth,
+    height: birdHeight,
+}
+
+
+window.onload = function () {
+  board = document.getElementById('board')
+  board.height = boardHeight
+  board.width = boardWidth
+  context = board.getContext('2d')
+
+  context
+}
